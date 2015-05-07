53cc84310f82b1a4d032cc0ef6c6792f3971a96a
lijunge <lijunge@lijungede.local>
2015-05-07 15:17:58 +0800

    test1

---
+++ Quartz2Demo1/Quartz2Demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quartz2Demo1
+//
+//  Created by lijunge on 15-5-7.
+//  Copyright (c) 2015年 lijunge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quartz2Demo1/Quartz2Demo1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quartz2Demo1
+//
+//  Created by lijunge on 15-5-7.
+//  Copyright (c) 2015年 lijunge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
